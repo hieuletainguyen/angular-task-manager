75a3464a1b4c924dd34b12aca5b697ccf0ed3a2f
Agiat Ikazinat <robotproject333@gmail.com>
2024-12-03 23:53:55 -0500

    complete a bit frontend fixing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '/',
+        component: LoginComponent,
+        title: "Login Page"
+    },
+    {
+        path: '/tasks',
+        component: TaskListComponent,
+        title: "Task Page"
+    }
+];
