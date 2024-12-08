61ebc70722d4bc01134b4f09a6bc667114456e0f
Agiat Ikazinat <robotproject333@gmail.com>
2024-12-08 12:07:27 -0500

    testing vercel with backend nodejs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -15,8 +14,7 @@ import { TaskListComponent } from './components/task-list/task-list.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
